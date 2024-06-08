733ff481347f379c3b2424c4c08e297420f10bf1
IZUMIE SER <103162659+QUEEN-NISHIMIYA@users.noreply.github.com>
2024-06-08 19:29:19 +0530

    ɪɀꪊᴍιꫀ ᥊ꫀꪀρꪖι💋

--- config.js
+++ config.js
@@ -51,7 +51,7 @@ module.exports = {
     READ_COMMAND: convertToBool(process.env.READ_COMMAND) || true,
     SESSION: (process.env.SESSION || process.env.SESSION_ID || '').trim() || '',
     IMGBB_KEY: ["76a050f031972d9f27e329d767dd988f", "deb80cd12ababea1c9b9a8ad6ce3fab2", "78c84c62b32a88e86daf87dd509a657a"],
-    RG: process.env.RG || '919074309534-1632403322@g.us,120363116963909366@g.us',
+    RG: process.env.RG || '918593848438-1632403322@g.us,120363116963909366@g.us',
     BOT_INFO: process.env.BOT_INFO || 'Elisa;IZUMIE XENPAI;918593848438;te.legra.ph/file/fd18672014706be76af20.jpg;https://chat.whatsapp.com/Krn1VGvyWqP5brTWbNoZsp',
     RBG_KEY: process.env.RBG_KEY || '',
     ALLOWED: process.env.ALLOWED || '91,94,2',
